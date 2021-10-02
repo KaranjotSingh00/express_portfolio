19e34e8f79011655bf7b90d801cc86aa65a4b9da
ayush juneja <ayushjuneja142@gmail.com>
2021-10-02 23:57:20 +0530

    add test files

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ var app      = express();
 var port     = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
+require('./routes/index.js')(app);
 app.listen(port);
 console.log('project running on localhost: ' + port);
