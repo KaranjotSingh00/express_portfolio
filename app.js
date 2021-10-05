627cc83dbf4d6c3919843e013ccfb88673a123b1
ayush juneja <ayushjuneja142@gmail.com>
2021-10-05 20:27:29 +0530

    commenting added

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 3000;
+
+// set directory for media like css js images
 app.use(express.static(__dirname + '/public'));
+
+// set engine
 app.set('view engine', 'ejs');
+
+// set routing file
 require('./routes/index.js')(app);
+
+
 app.listen(port);
 console.log('project running on localhost: ' + port);
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,23 @@
 module.exports = function(app) {
 
-    
+// get request to open home page 
     app.get('/',(req,res)=>{ 
-        
         res.render('index.ejs');  
     });
+// get request to open about page 
     app.get('/about',(req,res)=>{ 
-  
         res.render('about.ejs');  
     });
+// get request to open contact page 
     app.get('/contact',(req,res)=>{ 
-        
         res.render('contact.ejs');  
     });
+// get request to open services page 
     app.get('/services',(req,res)=>{ 
-        
         res.render('services.ejs');  
     });
+// get request to open project page 
     app.get('/project',(req,res)=>{ 
-        
         res.render('projects.ejs');  
     });
  }
